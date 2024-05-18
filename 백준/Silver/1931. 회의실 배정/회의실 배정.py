ee5652688ce10703dcdc2071eaf30b2d46c157f0
oneE <bestoneeee@naver.com>
2024-05-18 22:21:53 +0900

    [Silver I] Title: 회의실 배정, Time: 256 ms, Memory: 53948 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -2,8 +2,7 @@ import sys
 
 n=int(input())
 arr = []
-cnt = 0
-previous = 0
+
 
 for i in range(n):
     a,b = map(int,sys.stdin.readline().rstrip().split())
@@ -11,6 +10,8 @@ for i in range(n):
     
 arr.sort(key=lambda x : (x[1],x[0]))
 
+cnt = 0
+previous = 0
 for start, end in arr:
     if start>=previous:
         cnt+=1
