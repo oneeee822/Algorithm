c00535857ba966563ae1976f5b0d21524ac453f2
oneE <bestoneeee@naver.com>
2024-08-09 16:45:08 +0900

    Update 절댓값 힙.java

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,31 +1,37 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.io.IOException;
+import java.util.PriorityQueue;
 
-public class Main {
-
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int n = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> absq = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                if (Math.abs(o1) == Math.abs(o2)) return o1 - o2;
-                else return Math.abs(o1) - Math.abs(o2);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (o1,o2)->{
+                if(Math.abs(o1)==Math.abs(o2)){
+                    return o1>o2? 1: -1;
+                }
+                else{
+                    return Math.abs(o1)-Math.abs(o2);
+                }
             }
-        });
-        StringBuilder sb = new StringBuilder();
-		
-		for(int i = 0; i < n; i++) {
-			int num = Integer.parseInt(br.readLine());
-			if(num == 0) {
-				if(absq.isEmpty()) sb.append("0").append("\n");
-				else sb.append(absq.poll()).append("\n");
-			}else {
-				absq.offer(num);
-			}
-		}
-		System.out.println(sb);
+        );
+        
+        while(n-->0){
+            int x = Integer.parseInt(br.readLine());
+            if(x==0){
+                if(pq.isEmpty()){
+                    System.out.println("0");
+                }
+                else{
+                    System.out.println(pq.poll());
+                }
+            }
+            else{
+                pq.add(x);
+            }
+        }
     }
 }
