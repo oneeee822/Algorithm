447ec9219d5e1c65f3d3493ef9692995b8aee711
oneE <bestoneeee@naver.com>
2024-09-04 10:38:54 +0700

    [level 3] Title: N으로 표현, Time: 26.01 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int N, int number) {
+        if(N==number) return 1;
+        
+        HashSet<Integer> dp[] = new HashSet[9];
+        
+        for(int i=1; i<9; i++){
+            dp[i]= new HashSet<>();
+        }
+        
+        for(int i=1; i<9; i++){
+            dp[i].add(Integer.parseInt(String.valueOf(N).repeat(i)));
+            
+            for(int j=1; j<i; j++){
+                for(int a : dp[j]){
+                    for(int b : dp[i-j]){
+                        dp[i].add(a+b);
+                        dp[i].add(a-b);
+                        dp[i].add(a*b);
+                        if(b!=0){
+                            dp[i].add(a/b);
+                        }
+                    }
+                }
+            }
+            
+            if(dp[i].contains(number))
+                return i;
+        }
+        
+        return -1;
+    }
+}
