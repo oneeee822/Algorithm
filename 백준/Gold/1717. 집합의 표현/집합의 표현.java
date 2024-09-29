14d4e7add92a6123d81db7a70f425997a76cf8fe
oneE <bestoneeee@naver.com>
2024-09-29 17:27:01 +0700

    [Gold V] Title: 집합의 표현, Time: 828 ms, Memory: 57744 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,57 +1,63 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
-public class Main {
-	static int N, M;
-	static int parent []; 
-	
-    public static int find(int x) {
-        if (x == parent[x]) {
-            return x;
+class Main{
+    static int N;
+    static int M;
+    static int[] parent;
+    
+    public static int find(int a){
+        if(parent[a]==a){
+            return a;
+        }else{
+            return parent[a]=find(parent[a]);
         }
-        return parent[x] = find(parent[x]);
     }
-
-    public static void union(int x, int y) {
-        x = find(x);
-        y = find(y);
-        if (x != y) {
-            if (x < y) {
-                parent[y] = x;
-            } else {
-                parent[x] = y;
+    
+    public static void union(int a, int b){
+        a = find(a);
+        b = find(b);
+        if(a!=b){
+            if(a<b){
+                parent[b]=a;
+            }
+            else{
+                parent[a]=b;
             }
         }
     }
-
-    public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		parent = new int[N+1];
-		
-		for (int i=0; i<N+1; i++) parent[i] = i; 
-		
-		for (int i=0; i<M; i++) {
-			st = new StringTokenizer(bf.readLine());
-			int plus = Integer.parseInt(st.nextToken());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			if (plus==0) {
-				union(a,b);
-			}
-			
-			else {
-				if(find(a)==find(b)) System.out.println("YES");
-				else System.out.println("NO");
-			}
-		}
-
-	}
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        parent = new int[N+1];
+        
+        for(int i=1; i<=N; i++){
+            parent[i]=i;
+        }
+        
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int op = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            if(op==0){
+                union(a,b);       
+            }
+            else{
+                if(find(a)==find(b)){
+                    System.out.println("YES");
+                }
+                else{
+                    System.out.println("NO");
+                }
+            }
+        }
+    }
 }
