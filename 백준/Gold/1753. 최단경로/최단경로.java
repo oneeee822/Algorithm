80d8ab58fce691d3728889b0359744256a680e52
oneE <bestoneeee@naver.com>
2024-09-29 20:46:17 +0900

    Update 최단경로.java

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,79 +1,86 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 
-public class Main {
-    static int V, E, K;
-    static ArrayList<Node>[] graph;
-    static boolean[] visited;
-    static int[] dijkstraT;
+class Node implements Comparable<Node>{
+    int end, weight;
 
-    static class Node {
-        int n;
-        int cost;
-
-        Node(int n, int cost) {
-            this.n = n;
-            this.cost = cost;
-        }
+    public Node(int end, int weight){
+        this.end = end;
+        this.weight = weight;
     }
 
+    @Override
+    public int compareTo(Node o) {
+        return weight - o.weight;
+    }
+}
+
+class Main{
+    static int V;
+    static int E;
+    static int K;
+    static List<Node>[] list;
+    static int[] dist;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(br.readLine());
-
-        graph = new ArrayList[V + 1];
-        for (int i = 1; i < V + 1; i++) graph[i] = new ArrayList<>();
-        for (int i = 0; i < E; i++) {
+        list = new ArrayList[V+1];
+        dist = new int[V+1];
+        
+        for(int i=1; i<=V; i++){
+            list[i]= new ArrayList<>();
+        }
+        Arrays.fill(dist, Integer.MAX_VALUE);
+    
+        for(int i = 0 ; i < E; i++){
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            graph[u].add(new Node(v, w)); // 최단 거리로 갱신
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            list[start].add(new Node(end, weight));
         }
-
+  
         dijkstra(K);
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i < V + 1; i++) {
-            if (i == K) {
-                sb.append(0).append("\n");
-            } else if (dijkstraT[i] == Integer.MAX_VALUE) {
-                sb.append("INF").append("\n");
-            } else {
-                sb.append(dijkstraT[i]).append("\n");
-            }
+        
+        StringBuilder sb = new StringBuilder();        
+        for(int i = 1; i <= V; i++){
+            if(dist[i] == Integer.MAX_VALUE) sb.append("INF\n");
+            else sb.append(dist[i] + "\n");
         }
+        
         System.out.print(sb);
     }
-
-    static void dijkstra(int s) {
-        visited = new boolean[V + 1];
-        dijkstraT = new int[V + 1];
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> {
-            return o1.cost - o2.cost;
-        });
-
-        Arrays.fill(dijkstraT, Integer.MAX_VALUE);
-        dijkstraT[s] = 0;
-        pq.add(new Node(s, 0));
-
-        while (!pq.isEmpty()) {
-            Node cur = pq.poll();
-            if (visited[cur.n]) continue;
-            visited[cur.n] = true;
-
-            for (Node node : graph[cur.n]) {
-                if (visited[node.n]) continue;
-                int newW = cur.cost + node.cost;
-                int originW = dijkstraT[node.n];
-                if (originW > newW) {
-                    dijkstraT[node.n] = newW;
-                    pq.add(new Node(node.n, newW));
+    
+    private static void dijkstra(int start){
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        boolean[] checked = new boolean[V+1];
+        pq.add(new Node(start,0));
+        dist[start] = 0;
+        
+        while(!pq.isEmpty()){
+            Node curNode = pq.poll();
+            int cur = curNode.end;
+            
+            if(checked[cur]) continue;
+            checked[cur] = true;
+            
+            for(Node n : list[cur]){
+                if(dist[n.end]> dist[cur]+n.weight){
+                    dist[n.end] = dist[cur]+n.weight;
+                    pq.add(new Node(n.end, dist[n.end]));
                 }
             }
         }
     }
+    
 }
