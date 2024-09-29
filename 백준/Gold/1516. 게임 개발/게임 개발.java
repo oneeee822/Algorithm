d6faa602efbae5bc0b573c7fa5fbd62abc26b600
oneE <bestoneeee@naver.com>
2024-09-29 19:19:43 +0700

    [Gold III] Title: 게임 개발, Time: 220 ms, Memory: 21704 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -1,66 +1,64 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Main {
-    static int N;
-    static List<List<Integer>> l = new ArrayList<>();
-    static int[] time;
-    static int[] pre;
-    static int[] answer;
-
-    public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-
-        time = new int[N+1];
-        pre = new int[N+1];
-        answer = new int[N+1];
-        for(int i = 0; i<=N; i++)
-            l.add(new ArrayList<>());
-
-        for(int i = 1; i<=N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            time[i] = Integer.parseInt(st.nextToken());
-
-            while(st.hasMoreTokens()){
-                int num = Integer.parseInt(st.nextToken());
-
-                if(num == -1)
-                    break;
-
-                l.get(num).add(i);
-                pre[i]++;
-            }
-        }
-
-        topologicalSort();
-
-        for(int i = 1; i<=N; i++)
-            System.out.println(answer[i]);
-    }
-
-    static void topologicalSort(){
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int time[] = new int[N + 1];
+		int indegree[] = new int[N + 1];
+		ArrayList<Integer> list[] = new ArrayList[N + 1];
         Queue<Integer> q = new LinkedList<>();
-
-        for(int i = 1; i<=N; i++)
-            if(pre[i] == 0)
-                q.offer(i);
-
-        while(!q.isEmpty()){
-            int vertex = q.poll();
-            answer[vertex] += time[vertex];
-
-            for(int i = 0; i<l.get(vertex).size(); i++){
-                int nextV = l.get(vertex).get(i);
-
-                pre[nextV]--;
-                if(pre[nextV] == 0)
-                    q.offer(nextV);
-
-                answer[nextV] = Math.max(answer[nextV], answer[vertex]);
+        int[] result = new int[N+1];
+        
+        for(int i=1; i<=N; i++){
+            list[i] = new ArrayList<>();
+        }
+        
+        for(int i=1; i<=N; i++){
+            st = new StringTokenizer(br.readLine());
+            time[i] = Integer.parseInt(st.nextToken());
+            
+            while(st.hasMoreTokens()){
+                int pre = Integer.parseInt(st.nextToken());
+                
+                if(pre==-1)
+                    break;
+                
+                indegree[i]++;
+                list[pre].add(i);
             }
         }
+        
+        for(int i=1; i<=N; i++){
+            if(indegree[i]==0){
+                q.offer(i);
+                result[i]=time[i];
+            }
+        }
+        
+        while(!q.isEmpty()){
+            int idx = q.poll();
+            
+            for(int i : list[idx]){
+                result[i] = Math.max(result[i], time[i]+result[idx]);
+                indegree[i]--;
+                if(indegree[i]==0)
+                    q.offer(i);
+            }
+        }
+        
+        for(int i=1;i<=N;i++)
+			sb.append(result[i]).append('\n');
+		
+		System.out.println(sb);
     }
 }
