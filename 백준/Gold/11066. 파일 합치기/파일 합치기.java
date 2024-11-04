8fa098cc1a271cb6cb5f833b0ed0ede47d2943cf
OneE <bestoneeee@naver.com>
2024-11-05 00:53:14 +0900

    [Gold III] Title: 파일 합치기, Time: 460 ms, Memory: 28836 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.java
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -10,30 +10,26 @@ public class Main {
 		
         int T = Integer.parseInt(br.readLine());
         for(int t = 0; t<T; t++) {
-            
             int K = Integer.parseInt(br.readLine());
-            int[] files = new int[K+1];
-            int dp[][] = new int[K+1][K+1];
-
+            int[] Csum = new int[K+1];
+            int[][] dp = new int[K][K];
+            
             st = new StringTokenizer(br.readLine());
-            files[1] = Integer.parseInt(st.nextToken());
-            for(int i=2; i<=K; i++) {
-                int tmp = Integer.parseInt(st.nextToken());
-                files[i] = tmp+files[i-1];
+            for(int i=1; i<=K; i++){
+                Csum[i] = Integer.parseInt(st.nextToken()) + Csum[i-1];
             }
-
-
-            for(int gap=1; gap<K; gap++) {
-                for(int start = 1; start+gap <= K; start++) {
+        
+            for(int gap = 1; gap < K; gap++) {
+                for(int start = 0; start + gap < K; start++) {
                     int end = start + gap;
                     dp[start][end] = Integer.MAX_VALUE;
-
-                    for(int mid = start; mid<end ;mid++) {
-                        dp[start][end] = Math.min(dp[start][end],dp[start][mid]+dp[mid+1][end]+files[end]-files[start-1]);
+                    for(int mid = start; mid < end; mid++) {
+                        dp[start][end] = Math.min(dp[start][end],
+                                dp[start][mid] + dp[mid + 1][end] + Csum[end + 1] - Csum[start]);
                     }
                 }
             }
-            System.out.println(dp[1][K]);
+            System.out.println(dp[0][K-1]);
         }
     }
 }
