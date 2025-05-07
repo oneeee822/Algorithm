2b755cdd383090515aa4f4ba2d7d79cbf482b9b9
OneE <bestoneeee@naver.com>
2025-05-07 15:15:36 +0900

    Update 1， 2， 3 더하기 3.java

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -1,32 +1,36 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
- 
-public class Main {
-	
-	public static int N, T;
-	public static long[] dp;
-	public static int answer;
-	public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	
-    	dp = new long[1000001];
-    	T = Integer.parseInt(st.nextToken());
-    	
-    	dp[1] = 1;
-    	dp[2] = 2;
-    	dp[3] = 4;
-    	dp[4] = dp[4-1] + dp[4-2] + dp[4-3];
-    	for(int i=4; i<1000001;i++) {
-    		dp[i] = ( dp[i-1] + dp[i-2] + dp[i-3] ) % 1000000009;
-    	}
-    	
-    	for(int t=0;t<T;t++) {
-    		st = new StringTokenizer(br.readLine());
-    		N = Integer.parseInt(st.nextToken());
-    		System.out.println(dp[N] % 1000000009);
-    	}
-    }
+import java.io.IOException;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		for(int j=0; j<t; j++){
+		    int n = Integer.parseInt(br.readLine());
+		    
+		    if (n == 1) {
+                System.out.println(1);
+                continue;
+            } else if (n == 2) {
+                System.out.println(2);
+                continue;
+            } else if (n == 3) {
+                System.out.println(4);
+                continue;
+            }
+            
+		    long [] dp = new long[n+1];
+		    
+		    dp[0] = 1;
+		    dp[1] = 2;
+		    dp[2] = 4;
+ 		    for(int i=3; i<n; i++){
+		        dp[i] = (dp[i-1] + dp[i-2] + dp[i-3])%1000000009 ;
+		    }
+		    System.out.println(dp[n-1]);
+		}
+	}
 }
