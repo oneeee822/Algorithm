682dead755997cdf56390301b0975c42497adcdb
OneE <bestoneeee@naver.com>
2025-09-08 02:03:28 +0900

    [level 3] Title: 숫자 게임, Time: 106.70 ms, Memory: 67 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        int i = 0, j = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        while(i<A.length && j<B.length){
+            if(A[i]<B[j]){
+                answer ++;
+                i++;
+                j++;
+            }
+            else {
+                j++;
+                if(j < B.length){
+                    if(A[i]==B[j]) continue;
+                    else if(A[i]<B[j]){
+                        answer++;
+                        i++;
+                        j++;
+                    }
+                }
+            }
+            
+        }
+        return answer;
+    }
+}
