86321a9753353ac86f529974246f353cc8d1296f
OneE <bestoneeee@naver.com>
2025-09-21 23:53:40 +0900

    [level 3] Title: 불량 사용자, Time: 230.46 ms, Memory: 188 MB -BaekjoonHub

---
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Solution {
+    static boolean[] check;
+    static HashSet<String> set;
+    
+	public static int solution(String[] userid, String[] banid) {
+        check = new boolean[userid.length];
+        set = new HashSet<String>();
+        
+
+        for(int i=0; i<banid.length; i++) 
+        	banid[i] = banid[i].replace('*', '.');
+        
+        dfs(0,"",banid,userid);
+        
+		return set.size();
+	}
+	
+	public static void dfs(int depth, String res, String[] banid, String[] userid) {
+		if(depth==banid.length) {
+			String[] arr = res.split(" ");
+			Arrays.sort(arr);
+			
+			String str="";
+			for(String s:arr) str+=s;
+			set.add(str);
+			
+			return;
+		}
+		
+		for(int i=0; i<userid.length; i++) {
+
+			if(check[i] || !userid[i].matches(banid[depth])) continue;			
+			check[i]=true;
+			dfs(depth+1, userid[i]+" "+res, banid, userid);
+			check[i]=false;
+		}
+	}
+}
