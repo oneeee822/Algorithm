d5a0f4efc17e740b430b49c2ebd8b206acba3915
OneE <bestoneeee@naver.com>
2025-11-14 00:21:45 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.89 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,48 +1,42 @@
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.*;
 
 class Solution {
     int[] dx = {1, -1, 0, 0};
     int[] dy = {0, 0, -1, 1};
     
     public int solution(int[][] maps) {
-        int answer = 0;
-        int[][] visited = new int[maps.length][maps[0].length];
-        
-        bfs(maps, visited);
-        answer = visited[maps.length-1][maps[0].length-1];
-        
-        if(answer == 0)
-            answer = -1;
-        
-        return answer;
-    }
-    
-    private void bfs(int[][] maps, int[][] visited){
-        int x = 0;
-        int y = 0;
-        visited[x][y] = 1;
+        int n = maps.length;
+        int m = maps[0].length;
+        int[][] distance = new int[n][m];
+        boolean[][] visited = new boolean[n][m];
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{x,y});
+        
+        q.add(new int[] {0,0});
+        visited[0][0] = true;
+        distance[0][0] = 1;
         
         while(!q.isEmpty()){
-            int[] current = q.remove();
-            int cX = current[0];
-            int cY = current[1];
+            int[] now = q.poll();
             
-            for(int i = 0; i<4; i++){
-                int nX = cX + dx[i];
-                int nY = cY + dy[i];
+            int nx = now[0];
+            int ny = now[1];
+            
+            for(int i=0; i<4; i++){
+                int x = nx + dx[i];
+                int y = ny + dy[i];
                 
-                if(nX<0 || nX>maps.length-1 || nY<0 || nY>maps[0].length-1)
-                    continue;
+                if(x<0 || x>n-1 || y<0 || y>m-1 || maps[x][y]==0 || visited[x][y])
+                continue;
                 
-                if(visited[nX][nY]==0 && maps[nX][nY]==1){
-                    visited[nX][nY] = visited[cX][cY] + 1;
-                    q.add(new int[]{nX,nY});
-                }
+                q.add(new int[] {x,y});
+                visited[x][y] = true;
+                distance[x][y] = distance[nx][ny]+1;
             }
         }
         
+        if(distance[n-1][m-1]==0)
+            return -1;
+        else
+            return distance[n-1][m-1];
     }
 }
