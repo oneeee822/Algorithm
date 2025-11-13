ad8732ccaee514fd054e3606ae59b625ee8e5a7e
OneE <bestoneeee@naver.com>
2025-11-13 23:34:48 +0900

    [level 2] Title: 타겟 넘버, Time: 6.68 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,19 +1,21 @@
 class Solution {
     int answer = 0;
+    
     public int solution(int[] numbers, int target) {
-        dfs(0,numbers,target,0);
+        
+        dfs(0, target, 0, numbers);
+
         return answer;
     }
     
-    private void dfs(int depth, int[] numbers, int target, int sum){
-        if(depth == numbers.length){
-            if(sum == target){
-                answer ++;
-            }
+    public void dfs(int i, int target, int result, int[] numbers){
+        if(i == numbers.length){
+            if(result==target)
+                answer++;
             return;
         }
         
-        dfs(depth+1, numbers, target, sum+numbers[depth]);
-        dfs(depth+1, numbers, target, sum-numbers[depth]);
+        dfs(i+1, target, result+numbers[i], numbers);
+        dfs(i+1, target, result-numbers[i], numbers);
     }
 }
