f264be13b6d5d1fcfd437c74490b32d3adb716b4
OneE <bestoneeee@naver.com>
2025-11-13 18:25:16 +0900

    [level 2] Title: 올바른 괄호, Time: 20.22 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,25 +1,25 @@
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
 
         Stack<Character> st = new Stack<>();
-        for (char x : s.toCharArray()) {
-            if (x == '(') {
-                st.push(x);
-            } else {
-                if (st.isEmpty() || st.pop() != '(') {
-                    answer = false;
-                    break;
-                }
+        
+        for(char c : s.toCharArray()){
+            if(c=='('){
+                st.push(c);
+            }
+            else{
+                if(st.isEmpty())
+                    return false;
+                st.pop();
             }
         }
-        
-        if(!st.isEmpty()){
+            
+        if(!st.isEmpty())
             answer = false;
-        }
-    
+        
         return answer;
     }
 }
