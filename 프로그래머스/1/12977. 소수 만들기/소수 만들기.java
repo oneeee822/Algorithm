047c1824e4b8415ac12d344f1e03f69e71b83045
WONHEE JEONG <bestoneeee@naver.com>
2026-01-31 23:11:37 +0900

    [level 1] Title: 소수 만들기, Time: 2.45 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int cnt = 0;
+
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                for(int k=j+1; k<nums.length; k++){
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if(isPrime(sum)) cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+    
+    public boolean isPrime(int sum){
+        for(int i=2; i*i<=sum; i++){
+            if(sum%i==0){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
