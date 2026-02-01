67081fcd1997dcbc561253fd63c4c641132351c8
WONHEE JEONG <bestoneeee@naver.com>
2026-02-01 14:21:01 +0900

    [level 3] Title: 네트워크, Time: 0.24 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,28 +1,26 @@
+import java.util.*;
+
 class Solution {
+    public static int cnt = 0;
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        boolean[] visited = new boolean[computers.length];
+        cnt = 0;
+        boolean[] visited = new boolean[n];
         
-        for(int i = 0; i < computers.length; i++){
-            visited[i] = false;
+        for(int i=0; i<computers.length; i++){
+            if(!visited[i]) {
+                cnt++;
+                dfs(visited, computers, i);
+            }
         }
         
-        for(int i = 0; i < computers.length; i++){
-            if(visited[i] == false){
-                answer++;
-                dfs(i, visited, computers);
-            }            
-        }
-        
-        return answer;
+        return cnt;
     }
     
-    public void dfs(int node, boolean[] visited, int[][] computers){
-        visited[node] = true;
-        
-        for(int i = 0; i < computers.length; i++){
-            if(visited[i] == false && computers[node][i] == 1){
-                dfs(i, visited, computers);
+    public void dfs(boolean[] visited, int[][] computers, int i){
+        visited[i]=true;
+        for(int k=0; k<computers.length; k++){
+            if(computers[i][k]==1 && !visited[k]) {
+                dfs(visited, computers, k);
             }
         }
     }
