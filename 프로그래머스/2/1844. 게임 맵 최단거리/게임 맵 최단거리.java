ab179f6ec54c73e6d6c76c6f8f67e66de1bc68c7
WONHEE JEONG <bestoneeee@naver.com>
2026-02-01 13:22:16 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.79 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,42 +1,38 @@
 import java.util.*;
 
 class Solution {
-    int[] dx = {1, -1, 0, 0};
-    int[] dy = {0, 0, -1, 1};
-    
+    public int[] dx = {1,-1,0,0};
+    public int[] dy = {0,0,-1,1};
     public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
-        int[][] distance = new int[n][m];
-        boolean[][] visited = new boolean[n][m];
-        Queue<int[]> q = new LinkedList<>();
-        
-        q.add(new int[] {0,0});
-        visited[0][0] = true;
-        distance[0][0] = 1;
+        int answer = 0;
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
+        int[][] way = new int[maps.length][maps[0].length];
+            
+        bfs(maps, visited, way);
+        return (way[maps.length-1][maps[0].length-1]==0) ? -1:way[maps.length-1][maps[0].length-1];
+    }
+    
+    public void bfs(int[][] maps, boolean[][] visited, int[][] way){
+        Deque<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{0,0});
+        way[0][0] = 1;
         
         while(!q.isEmpty()){
-            int[] now = q.poll();
-            
-            int nx = now[0];
-            int ny = now[1];
+            int[] cur = q.poll();
             
             for(int i=0; i<4; i++){
-                int x = nx + dx[i];
-                int y = ny + dy[i];
+                int x = cur[0] + dx[i];
+                int y = cur[1] + dy[i];
                 
-                if(x<0 || x>n-1 || y<0 || y>m-1 || maps[x][y]==0 || visited[x][y])
-                continue;
+                if(x>=maps.length || x<0 ||y>=maps[0].length|| y<0)
+                    continue;
+                if(visited[x][y]) continue;
+                if(maps[x][y] == 0) continue;
                 
-                q.add(new int[] {x,y});
+                q.add(new int[]{x, y});
                 visited[x][y] = true;
-                distance[x][y] = distance[nx][ny]+1;
+                way[x][y] = way[cur[0]][cur[1]] + 1;
             }
         }
-        
-        if(distance[n-1][m-1]==0)
-            return -1;
-        else
-            return distance[n-1][m-1];
     }
 }
