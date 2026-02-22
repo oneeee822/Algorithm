7bcab56d704050a8eebd8510ee1dcdf54555d5bc
OneE <bestoneeee@naver.com>
2026-02-22 14:25:42 +0900

    [level 1] Title: 최소직사각형, Time: 6.99 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/README.md
+++ 프로그래머스/1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=java) 
 
 ### 성능 요약
 
-메모리: 91.1 MB, 시간: 2.13 ms
+메모리: 108 MB, 시간: 6.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 19일 13:59:28
+2026년 02월 22일 14:25:40
 
 ### 문제 설명
 
--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,18 +1,24 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int[][] sizes) {
-        int maxWidth = 0;
-        int maxHeight = 0;
-
-        for (int[] size : sizes) {
-            int w = Math.max(size[0], size[1]);
-            int h = Math.min(size[0], size[1]);
-
-            maxWidth = Math.max(maxWidth, w);
-            maxHeight = Math.max(maxHeight, h);
+        HashSet<Integer> wSet = new HashSet<>();
+        HashSet<Integer> hSet = new HashSet<>();
+        
+        for(int[] size : sizes){
+            if(size[0]>size[1]){
+                wSet.add(size[0]);
+                hSet.add(size[1]);
+            }
+            else{
+                wSet.add(size[1]);
+                hSet.add(size[0]);
+            }
         }
-
-        return maxWidth * maxHeight;
+        
+        int w = Collections.max(wSet);
+        int h = Collections.max(hSet);
+        
+        return w*h;
     }
 }
