48b9b5feaa834d015e00050ea14b6a81ae006e82
OneE <bestoneeee@naver.com>
2026-02-25 21:48:36 +0900

    [level 2] Title: 피로도, Time: 3.23 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,29 +1,28 @@
+import java.util.*;
+
 class Solution {
-    int answer = 0;
-    boolean[] visited = new boolean[9];
+    static boolean visited[];
+    static int answer = 0;
     public int solution(int k, int[][] dungeons) {
-        
-        for(int i=0; i<dungeons.length; i++)
-            dfs(i, 0, k, dungeons);
+
+        visited = new boolean[dungeons.length];
+        dfs(k, dungeons, 0);
         
         return answer;
     }
     
-    public void dfs(int i, int depth, int k, int[][] dungeons){
-        if(k<dungeons[i][0]){
-            answer = Math.max(answer, depth);
-            return;
-        }
-        
-        visited[i] = true;
-        for(int j=0; j<dungeons.length; j++){
-            if(!visited[j]){
-                dfs(j, depth+1, k-dungeons[i][1], dungeons);
+    public void dfs(int cur, int[][] dungeons, int cnt){
+        answer = Math.max(cnt, answer);
+
+        for(int i=0; i<dungeons.length; i++){
+            if(visited[i] == true) continue; 
+            
+            if(dungeons[i][0]<=cur){
+                visited[i] = true;
+                dfs(cur-dungeons[i][1], dungeons, cnt+1);
+                visited[i] = false;
             }
         }
-        
-        visited[i] = false;
-        answer = Math.max(answer, depth+1);
         
         
     }
