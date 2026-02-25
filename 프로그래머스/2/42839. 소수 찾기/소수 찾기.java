fa545d9195ea76eed01a95ffbdd77a74869618b6
OneE <bestoneeee@naver.com>
2026-02-25 20:34:52 +0900

    [level 2] Title: 소수 찾기, Time: 100.85 ms, Memory: 92.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,40 +1,41 @@
 import java.util.*;
 
 class Solution {
-    HashSet<Integer> numbersSet = new HashSet<>();
-    public int solution(String numbers) {
-        
-        dfs("", numbers);
+    static Set<Integer> answer = new HashSet<>();
+    static boolean[] visited;
 
-        int count = 0;
-        Iterator<Integer> it = numbersSet.iterator();
+    public int solution(String numbers) {
+        String[] numList = numbers.split("");
         
-        while (it.hasNext()) {
-            int number = it.next();
-            if (isPrime(number))
-                count++;
+        for(int i=0; i<numList.length; i++){
+            visited = new boolean[numList.length];
+            
+            visited[i] = true;
+            dfs(numList, numList[i]);
         }
-        
-        return count;
+            
+        return answer.size();
     }
     
-    public boolean isPrime(int num) {
-        if (num == 0 || num == 1)
-            return false;
-
-        for (int i = 2; i <= (int)Math.sqrt(num); i++)
-            if (num % i == 0)
-                return false;
-
-        return true;
+    public void dfs(String[] numList, String num){
+        checkPrime(Integer.parseInt(num));
+ 
+        for(int i=0; i<numList.length; i++){
+            if(visited[i]) continue;
+            visited[i] = true;
+            dfs(numList, num+numList[i]);
+            visited[i] = false;
+        }
     }
-
-    public void dfs(String str, String numbers) {
-        if (!str.equals(""))
-            numbersSet.add(Integer.valueOf(str));
-
-        for (int i = 0; i < numbers.length(); i++)
-            dfs(str + numbers.charAt(i), numbers.substring(0, i) + numbers.substring(i + 1));
-
+    
+    // 소수인지 확인
+    public void checkPrime(int n){
+        if(n<2) return;
+        
+        for(int i=2; i<n; i++){
+            if(n%i==0) return;
+        }
+        
+        answer.add(n);
     }
 }
