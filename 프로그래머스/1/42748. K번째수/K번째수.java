ad15a049a9c41adbb23ef6a759d055b442afb023
OneE <bestoneeee@naver.com>
2026-02-27 23:47:39 +0900

    [level 1] Title: K번째수, Time: 0.37 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,17 +4,12 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
-        for(int i = 0; i<commands.length; i++){
-            int k = 0;
-            
-            int[] arr = new int[commands[i][1]-commands[i][0]+1];
-            for(int j = commands[i][0]-1; j<commands[i][1]; j++){
-                arr[k++] = array[j];
-            }
-            
-            Arrays.sort(arr);
-            answer[i]=arr[commands[i][2]-1];
+        for(int i=0; i<commands.length; i++){
+            int[] list = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(list);
+            answer[i] = list[commands[i][2]-1];
         }
+        
         return answer;
     }
 }
