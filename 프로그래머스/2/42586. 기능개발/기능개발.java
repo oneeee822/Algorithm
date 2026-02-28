fc38eafe5fe001c06b031499cb207eb8e035fe11
OneE <bestoneeee@naver.com>
2026-02-28 13:01:53 +0900

    [level 2] Title: 기능개발, Time: 4.30 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,41 +1,30 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
+import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         List<Integer> answer = new ArrayList<>();
-        Queue<Integer> q = new LinkedList<Integer>();
-        int day = 0 ;
-        
-        for(int i = 0; i< progresses.length; i++){
-            if((100-progresses[i])%speeds[i]==0){
-                day = (100-progresses[i])/speeds[i];
-            }
-            else{
-                day = (100-progresses[i])/speeds[i] + 1;
-            }
-            
-            q.add(day);
+        int[] days = new int[progresses.length];
+
+        for(int i=0; i<progresses.length; i++){
+            days[i] = (100-progresses[i]+speeds[i]-1)/speeds[i];
         }
-        
-        int functions = 0;
-        day = q.peek();
-        
-        while(!q.isEmpty()){
-            
-            if(q.peek()<=day){
-                functions++;
-                q.poll();
+
+        int current = days[0];
+        int cnt = 1;
+
+        for(int i=1; i<days.length; i++){
+            if(days[i] <= current){
+                cnt++;
+            } else {
+                answer.add(cnt);
+                current = days[i];
+                cnt = 1;
             }
-            else {
-				answer.add(functions);
-				functions = 1;
-				day = q.poll(); 
-			}
         }
-        answer.add(functions);
-        return answer.stream().mapToInt(i->i).toArray();
+
+        answer.add(cnt);
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
